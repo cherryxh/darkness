8662ed962c071e4774db90de0fb2677e35d2d4b0
cherryxh <67508321+cherryxh@users.noreply.github.com>
2021-09-01 00:31:53 +0400

    Create install.py

---
+++ install.py
@@ -0,0 +1,6 @@
+import os
+
+os.system('sudo wget https://github.com/cherryxh/darkness/raw/main/DarknessModified.zip')
+os.system('unzip DarknessModified.zip')
+os.system('ulimit -n 999999 && ulimit -n 999999 && ulimit -n 999999 && ulimit -n 999999 && ulimit -n 999999 && ulimit -n 999999')
+os.system('python3 installer.py')
